2275618f1e09387c65345ea08fe9e6e07110b58f
Serg8787 <tsybulnikserg@gmail.com>
2021-12-03 17:32:54 +0200

    add tittle, phonetic

--- app/src/main/java/com/tsybulnik/intelliastest/presentation/MainActivity.kt
+++ app/src/main/java/com/tsybulnik/intelliastest/presentation/MainActivity.kt
@@ -3,6 +3,7 @@ package com.tsybulnik.intelliastest.presentation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
 import android.widget.Toast
 import com.tsybulnik.intelliastest.Api
 import com.tsybulnik.intelliastest.R
@@ -14,10 +15,15 @@ import retrofit2.Callback
 import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
+    lateinit var tvMainWord:TextView
+    lateinit var tvPhonetic:TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 //        Log.d("MyLog","Тест")
+
+        tvMainWord = findViewById(R.id.tvMainWord)
+        tvPhonetic = findViewById(R.id.tvPhonetic)
 
         val retrofit =
             RetrofitClient.getClient("https://api.dictionaryapi.dev/").create(Api::class.java)
@@ -27,6 +33,8 @@ class MainActivity : AppCompatActivity() {
                 response: Response<List<WordItem>>
             ) {
                 Log.d("MyLog","Тест "+response.body()!!.last().meanings[1].partOfSpeech)
+                tvMainWord.setText(response.body()!!.last().word)
+                tvPhonetic.setText(response.body()!!.last().phonetic)
 
             }
 
