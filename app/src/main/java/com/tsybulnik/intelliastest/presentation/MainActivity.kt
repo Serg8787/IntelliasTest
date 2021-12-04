4d4eb8f0c4532e03f1e7020decf2901e3e72b6fa
Serg8787 <tsybulnikserg@gmail.com>
2021-12-04 12:38:49 +0200

    add array list and check in correct word

--- app/src/main/java/com/tsybulnik/intelliastest/presentation/MainActivity.kt
+++ app/src/main/java/com/tsybulnik/intelliastest/presentation/MainActivity.kt
@@ -1,23 +1,21 @@
 package com.tsybulnik.intelliastest.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.tsybulnik.intelliastest.Api
 import com.tsybulnik.intelliastest.R
 import com.tsybulnik.intelliastest.RetrofitClient
-import com.tsybulnik.intelliastest.domain.entities.Meaning
-import com.tsybulnik.intelliastest.domain.entities.Word
 import com.tsybulnik.intelliastest.domain.entities.WordItem
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
-    lateinit var tvMainWord:TextView
-    lateinit var tvPhonetic:TextView
+    lateinit var tvMainWord: TextView
+    lateinit var tvPhonetic: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -33,26 +31,42 @@ class MainActivity : AppCompatActivity() {
                 call: Call<List<WordItem>>,
                 response: Response<List<WordItem>>
             ) {
-                Log.d("MyLog", "Тест " + response.body()!!.last().meanings[1].definitions)
-                Log.d("MyLog", "meanings.size " + response.body()!!.last().meanings.size)
-                Log.d("MyLog", "meanings.toHashSet() " + response.body()!!.last().meanings[0].partOfSpeech)
-                Log.d("MyLog", "meanings.toHashSet() " + response.body()!!.last().meanings[0].definitions[0].definition)
-                val size:Int = response.body()!!.last().meanings.size
-                for(i in 0..size){
-                    val map = mutableMapOf<String,String>()
-
-                    map.put(response.body()!!.last().meanings[i].partOfSpeech,response.body()!!.last().meanings[i].definitions[i].definition)
-                    Log.d("MyLog", "map " + map.toString() )
-                }
+//                Log.d("MyLog", "Тест " + response.body()!!.last().meanings[1].definitions)
+//                Log.d("MyLog", "meanings.size " + response.body()!!.last().meanings.size)
+//                Log.d("MyLog", "meanings.toHashSet() " + response.body()!!.last().meanings[0].partOfSpeech)
+//                Log.d("MyLog", "meanings.toHashSet() " + response.body()!!.last().meanings[0].definitions[0].definition)
+                Log.d("MyLog", response.isSuccessful.toString())
+                if (response.isSuccessful) {
+                    val size: Int = response.body()!!.last().meanings.size
+//                Log.d("MyLog",size.toString())
+                    for (i in 0 until size) {
+                        val arrayPartOfSpeech = arrayOfNulls<String?>(size)
+                        for (i in 0 until size) {
+                            arrayPartOfSpeech[i] = response.body()!!.last().meanings[i].partOfSpeech
+                        }
+                        Log.d("MyLog", "arrayPartOfSpeech " + arrayPartOfSpeech[i].toString())
+                    }
+                    for (i in 0 until size) {
+                        val arraydefinition = arrayOfNulls<String?>(size)
+//                    Log.d("MyLog", "i"+i.toString() )
+                        for (i in 0 until size) {
+                            arraydefinition[i] =
+                                response.body()!!.last().meanings[i].definitions[0].definition
+                        }
+                        Log.d("MyLog", "arraydefinition " + arraydefinition[i].toString())
+                    }
 //
-
                 tvMainWord.setText(response.body()!!.last().word)
                 tvPhonetic.setText(response.body()!!.last().phonetic)
 
+                } else {
+                Toast.makeText(applicationContext,"Введите кор. слово",Toast.LENGTH_LONG).show()
+
+                }
             }
 
             override fun onFailure(call: Call<List<WordItem>>, t: Throwable) {
-                TODO("Not yet implemented")
+                Toast.makeText(applicationContext, "Ошибка", Toast.LENGTH_LONG).show()
             }
 
         })
