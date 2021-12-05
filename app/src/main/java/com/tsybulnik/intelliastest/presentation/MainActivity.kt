8bb40bc5c9a84895519dc1464894e1d093750f4d
Serg8787 <tsybulnikserg@gmail.com>
2021-12-05 15:00:27 +0200

    add player

--- app/src/main/java/com/tsybulnik/intelliastest/presentation/MainActivity.kt
+++ app/src/main/java/com/tsybulnik/intelliastest/presentation/MainActivity.kt
@@ -1,9 +1,10 @@
 package com.tsybulnik.intelliastest.presentation
 
+import android.media.AudioAttributes
+import android.media.MediaPlayer
 import android.os.Bundle
 import android.util.Log
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.tsybulnik.intelliastest.Api
@@ -15,57 +16,96 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
+
 class MainActivity : AppCompatActivity() {
     lateinit var tvMainWord: TextView
     lateinit var tvPhonetic: TextView
+    lateinit var btSound: ImageButton
+    lateinit var btSearch: Button
+    lateinit var etWord: EditText
+    lateinit var uri: String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         tvMainWord = findViewById(R.id.tvMainWord)
         tvPhonetic = findViewById(R.id.tvPhonetic)
+        btSound = findViewById(R.id.btSound)
+        etWord = findViewById(R.id.etEnterWord)
         val recyclerView: RecyclerView = findViewById(R.id.recycler)
 
-        val retrofit =
-            RetrofitClient.getClient("https://api.dictionaryapi.dev/").create(Api::class.java)
-        retrofit.getDataFromWord("hello").enqueue(object : Callback<List<WordItem>> {
-            override fun onResponse(
-                call: Call<List<WordItem>>,
-                response: Response<List<WordItem>> ) {
-                if (response.isSuccessful) {
-                val size: Int = response.body()!!.last().meanings.size
-                Log.d("MyLog", response.isSuccessful.toString())
-                val arrayPartOfSpeech = arrayOfNulls<String?>(size)
-                val arraydefinition = arrayOfNulls<String?>(size)
-                    for (i in 0 until size) {
+        btSearch = findViewById(R.id.btSearch)
+        btSearch.setOnClickListener {
+            val word: String = etWord.text.toString()
+            val retrofit =
+                RetrofitClient.getClient("https://api.dictionaryapi.dev/").create(Api::class.java)
+            retrofit.getDataFromWord(word).enqueue(object : Callback<List<WordItem>> {
+                override fun onResponse(
+                    call: Call<List<WordItem>>,
+                    response: Response<List<WordItem>>
+                ) {
+                    if (response.isSuccessful) {
+                        val size: Int = response.body()!!.last().meanings.size
+                        Log.d("MyLog", response.isSuccessful.toString())
+                        val arrayPartOfSpeech = arrayOfNulls<String?>(size)
+                        val arraydefinition = arrayOfNulls<String?>(size)
                         for (i in 0 until size) {
-                            arrayPartOfSpeech[i] = response.body()!!.last().meanings[i].partOfSpeech
+                            for (i in 0 until size) {
+                                arrayPartOfSpeech[i] =
+                                    response.body()!!.last().meanings[i].partOfSpeech
+                            }
+                            Log.d("MyLog", "arrayPartOfSpeech " + arrayPartOfSpeech[i].toString())
                         }
-                        Log.d("MyLog", "arrayPartOfSpeech " + arrayPartOfSpeech[i].toString())
-                    }
-                    for (i in 0 until size) {
                         for (i in 0 until size) {
-                            arraydefinition[i] =
-                                response.body()!!.last().meanings[i].definitions[0].definition
+                            for (i in 0 until size) {
+                                arraydefinition[i] =
+                                    response.body()!!.last().meanings[i].definitions[0].definition
+                            }
+                            Log.d("MyLog", "arraydefinition " + arraydefinition[i].toString())
                         }
-                        Log.d("MyLog", "arraydefinition " + arraydefinition[i].toString())
-                    }
-                    val map: Map<String?, String?> =
-                        arrayPartOfSpeech.zip(arraydefinition)
-                            .toMap()
-                    Log.d("MyLog", "map " + map)
-                    recyclerView.adapter = WordAdapter(map)
-                    tvMainWord.setText(response.body()!!.last().word)
-                    tvPhonetic.setText(response.body()!!.last().phonetic)
+                        val map: Map<String?, String?> =
+                            arrayPartOfSpeech.zip(arraydefinition)
+                                .toMap()
+                        Log.d("MyLog", "map " + map)
+                        recyclerView.adapter = WordAdapter(map)
+                        tvMainWord.setText(response.body()!!.last().word)
+                        tvPhonetic.setText("[ ${response.body()!!.last().phonetic} ]")
+                        uri = "https:" + response.body()!!.last().phonetics[0].audio
 
-                } else {
-                    Toast.makeText(applicationContext, "Введите корректное слово", Toast.LENGTH_LONG)
-                        .show()
+                    } else {
+                        Toast.makeText(
+                            applicationContext,
+                            "Введите корректное слово",
+                            Toast.LENGTH_LONG
+                        )
+                            .show()
+                    }
                 }
-            }
-            override fun onFailure(call: Call<List<WordItem>>, t: Throwable) {
-                Toast.makeText(applicationContext, "Ошибка", Toast.LENGTH_LONG).show()
-            }
-        })
+                override fun onFailure(call: Call<List<WordItem>>, t: Throwable) {
+                    Toast.makeText(applicationContext, "Ошибка", Toast.LENGTH_LONG).show()
+                }
+            })
+            etWord.text.clear()
+        }
+
+        btSound.setOnClickListener {
+            playSound()
+        }
+    }
+
+    fun playSound() {
+        val url = uri
+        val mediaPlayer = MediaPlayer().apply {
+            setAudioAttributes(
+                AudioAttributes.Builder()
+                    .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                    .setUsage(AudioAttributes.USAGE_MEDIA)
+                    .build()
+            )
+            setDataSource(url)
+            prepare() // might take long! (for buffering, etc)
+            start()
+        }
+
     }
 }
