e0c65e00c321cfa3c5cd2a9c2ccce8615e92e833
Serg8787 <tsybulnikserg@gmail.com>
2021-12-06 15:36:22 +0200

    to viewmodel

--- app/src/main/java/com/tsybulnik/intelliastest/presentation/MainActivity.kt
+++ app/src/main/java/com/tsybulnik/intelliastest/presentation/MainActivity.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import android.media.AudioAttributes
 import android.media.MediaPlayer
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.Button
@@ -17,13 +16,7 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.RecyclerView
 import com.tsybulnik.intelliastest.R
-import com.tsybulnik.intelliastest.data.model.WordItem
-import com.tsybulnik.intelliastest.data.network.Api
-import com.tsybulnik.intelliastest.data.network.RetrofitClient
 import com.tsybulnik.intelliastest.presentation.adapters.WordAdapter
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 
 class MainActivity : AppCompatActivity() {
@@ -40,8 +33,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         vm = ViewModelProvider(this).get(MainViewModel::class.java)
 
-
-
         tvMainWord = findViewById(R.id.tvMainWord)
         tvPhonetic = findViewById(R.id.tvPhonetic)
         etWord = findViewById(R.id.etEnterWord)
@@ -50,11 +41,10 @@ class MainActivity : AppCompatActivity() {
 
         btSearch = findViewById(R.id.btSearch)
 
-
-
         btSearch.setOnClickListener {
             hideKeybord()
             val word: String = etWord.text.toString()
+            etWord.text.clear()
             vm.loadData(word)
             vm.mainWord.observe(this, Observer {
                 tvMainWord.text = it
@@ -66,104 +56,46 @@ class MainActivity : AppCompatActivity() {
                 recyclerView.adapter = WordAdapter(it)
             })
             vm.isPhonetic.observe(this, Observer {
-                if(it){
+                if (it) {
                     clPhonetic.visibility = View.INVISIBLE
                 } else {
                     clPhonetic.visibility = View.VISIBLE
                 }
             })
-            vm.errorRightWord.observe(this, Observer {
-                Toast.makeText(this,it,Toast.LENGTH_LONG).show()
-            })
-            vm.errorRightResonse.observe(this, Observer {
-                Toast.makeText(this,it,Toast.LENGTH_LONG).show()
-            })
-//            val retrofit =
-//                RetrofitClient.getClient("https://api.dictionaryapi.dev/").create(Api::class.java)
-//            retrofit.getDataFromWord(word).enqueue(object : Callback<List<WordItem>> {
-//                override fun onResponse(
-//                    call: Call<List<WordItem>>,
-//                    response: Response<List<WordItem>>
-//                ) {
-//                    if (response.isSuccessful) {
-////                        val size: Int = response.body()!!.last().meanings.size
-////                        val arrayPartOfSpeech = arrayOfNulls<String?>(size)
-////                        val arraydefinition = arrayOfNulls<String?>(size)
-////                        for (i in 0 until size) {
-////                            for (i in 0 until size) {
-////                                arrayPartOfSpeech[i] =
-////                                    response.body()!!.first().meanings[i].partOfSpeech
-////                            }
-////                        }
-////                        for (i in 0 until size) {
-////                            for (i in 0 until size) {
-////                                arraydefinition[i] =
-////                                    response.body()!!
-////                                        .first().meanings[i].definitions.first().definition
-////                            }
-////                        }
-////                        val map: Map<String?, String?> =
-////                            arrayPartOfSpeech.zip(arraydefinition)
-////                                .toMap()
-////                        recyclerView.adapter = WordAdapter(map)
-////                        tvMainWord.setText(response.body()!!.first().word)
-////                        tvPhonetic.setText("[ ${response.body()!!.first().phonetic} ]")
-//                        if (response.body()!!.first().phonetics.isNotEmpty()) {
-//                            clPhonetic.visibility = View.VISIBLE
-//                            uri = "https:" + response.body()!!.first().phonetics.first().audio
-//                        } else {
-//                            clPhonetic.visibility = View.INVISIBLE
-//                        }
-//                    } else {
-//                        Toast.makeText(
-//                            applicationContext,
-//                            "Введите корректное слово",
-//                            Toast.LENGTH_LONG
-//                        )
-//                            .show()
-//                    }
-//                }
-//
-//                override fun onFailure(call: Call<List<WordItem>>, t: Throwable) {
-//                    Toast.makeText(
-//                        applicationContext,
-//                        "Ошибка запроса! Возможно пропал интернет",
-//                        Toast.LENGTH_LONG
-//                    ).show()
-//                }
-//            })
-//            etWord.text.clear()
-//        }
-//        clPhonetic.setOnClickListener {
-//            playSound()
-//        }
-//        }
-
-//            fun playSound() {
-//                val url = uri
-//                val mediaPlayer = MediaPlayer().apply {
-//                    setAudioAttributes(
-//                        AudioAttributes.Builder()
-//                            .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
-//                            .setUsage(AudioAttributes.USAGE_MEDIA)
-//                            .build()
-//                    )
-//                    setDataSource(url)
-//                    prepare() // might take long! (for buffering, etc)
-//                    start()
-//                }
-//
-//            }
-//
 
         }
+        vm.errorRightResonse.observe(this, Observer {
+            Toast.makeText(this, it, Toast.LENGTH_LONG).show()
+        })
+        vm.errorRightWord.observe(this, Observer {
+            Toast.makeText(this, it, Toast.LENGTH_LONG).show()
+        })
+
+        clPhonetic.setOnClickListener {
+            playSound()
+        }
     }
-   private fun hideKeybord() {
+
+    private fun hideKeybord() {
         val view: View? = this.currentFocus
         if (view != null) {
             val imm: InputMethodManager =
                 getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
             imm.hideSoftInputFromWindow(view.getWindowToken(), 0)
+        }
+    }
+    fun playSound() {
+        val url = uri
+        val mediaPlayer = MediaPlayer().apply {
+            setAudioAttributes(
+                AudioAttributes.Builder()
+                    .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                    .setUsage(AudioAttributes.USAGE_MEDIA)
+                    .build()
+            )
+            setDataSource(url)
+            prepare() // might take long! (for buffering, etc)
+            start()
         }
     }
 }
